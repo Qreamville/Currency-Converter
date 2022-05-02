622dd41d4ccf10c6e78b5f864a69d8fc5af19a35
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-02 08:39:29 -1200

    Deleted files

--- src/components/ExchangeRate/ExchangeRate.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ExchangeRate = () => {
-  return <div className="bg-success">ExchangeRate</div>;
-};
-
-export default ExchangeRate;
--- src/components/NewsFeed/NewsFeed.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewsFeed = () => {
-  return <div className="newsFeed bg-primary">NewsFeed</div>;
-};
-
-export default NewsFeed;
