3ac5214e90d8eb3177fd8ee97e1bef89277384c4
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-02 08:40:54 -1200

    Updated js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Converter from "./components/Converter/Converter";
-import NewsFeed from "./components/NewsFeed/NewsFeed";
 
 function App() {
   return (
     <div className="App">
       <Converter />
-      <NewsFeed />
     </div>
   );
 }
